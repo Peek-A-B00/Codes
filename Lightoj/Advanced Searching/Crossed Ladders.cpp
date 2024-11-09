ff26c1b4b86749e6d530efaecf2053c58bfb982c
Peek-A-B00 <98767453+Peek-A-B00@users.noreply.github.com>
2024-11-09 08:46:55 +0600

    Add files via upload

---
+++ Lightoj/Advanced Searching/Crossed Ladders.cpp
@@ -0,0 +1,92 @@
+
+//#include<bits/stdC++.h>
+#include <stdio.h>
+#include <float.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include<iomanip>
+
+/** ---- Paknami Starts ----*/
+#include <iostream>
+#include<cmath>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include<bitset>
+#include <queue>
+#include <map>
+#include<set>
+#include <unordered_map>
+using namespace std;
+
+#define deb(a)     cout<<__LINE__<<"# "<<#a<<" -> "<<a<<endl;
+
+//For Debugging
+#define debug(a...)          {cout<<__LINE__<<"#--> ";dbg,a; cout<<endl;}
+struct debugger
+{
+    template<typename T> debugger& operator, (const T v)
+    {
+        cout<<v<<" ";
+        return *this;
+    }
+} dbg;
+
+#define en "\n"
+#define spf(d) setprecision(d)<<fixed
+
+/** ---- Paknami Ends ----*/
+
+/*
+    if(st.find(val)!=st.end()){}
+*/
+
+typedef long long LL;
+const LL MOD = 1000000007;///1e9+7
+const double EPS = 1e-7; ///1*10^-7
+const double pi = 2*acos(0.0);
+const int Size = 101;
+double x,y,c;
+double fun(double n)
+{
+    double a=acos(n/x);
+    double b=acos(n/y);
+    return (c/tan(a))+(c/tan(b));
+//    deb(n);
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    freopen("in.txt","r",stdin);
+//    freopen("out.txt","w",stdout);
+
+    int tks,ks=1;
+    cin>>tks;
+//    scanf("%d",&tks);
+    while(tks--)
+    {
+        cin>>x>>y>>c;
+        double l=0,r=1e9;
+        while(fabs(r-l)>EPS){
+            double mid=l+(r-l)/2;
+            if(mid>fun(mid))
+                l=mid;
+            else
+                r=mid;
+        }
+        cout<<"Case "<<ks++<<": "<<spf(10)<<r<<en;
+
+//        printf("Case %d: ",ks++);
+    }
+//
+//    while(1 == scanf("%d",&n) && n)
+//    {
+////        printf("\n");
+//    }
+    return 0;
+}
+
---
+++ Lightoj/Advanced Searching/Get the Containers.cpp
@@ -0,0 +1,118 @@
+
+
+//#include<bits/stdC++.h>
+#include <stdio.h>
+#include <float.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include<iomanip>
+
+/** ---- Paknami Starts ----*/
+#include <iostream>
+#include<cmath>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include<bitset>
+#include <queue>
+#include <map>
+#include<set>
+#include <unordered_map>
+using namespace std;
+
+#define deb(a)     cout<<__LINE__<<"# "<<#a<<" -> "<<a<<endl;
+
+//For Debugging
+#define debug(a...)          {cout<<__LINE__<<"#--> ";dbg,a; cout<<endl;}
+struct debugger
+{
+    template<typename T> debugger& operator, (const T v)
+    {
+        cout<<v<<" ";
+        return *this;
+    }
+} dbg;
+
+#define en "\n"
+#define spf(d) setprecision(d)<<fixed
+
+/** ---- Paknami Ends ----*/
+
+/*
+    if(st.find(val)!=st.end()){}
+*/
+
+typedef long long LL;
+const LL MOD = 1000000007;///1e9+7
+const double EPS = 1e-7; ///1*10^-7
+const double pi = 2*acos(0.0);
+const int Size = 1001;
+
+LL m,n;
+LL ar[Size];
+
+bool fun(LL x)
+{
+    LL c=1;
+    bool f=0;
+    LL t=x;
+    for(int i=0;i<n;i++)
+    {
+        if(x<ar[i])
+        {
+            f=1;break;
+        }
+        else{
+            if(t>=ar[i])
+            {
+                t-=ar[i];
+            }
+            else
+            {
+                t=x-ar[i];
+                c++;
+            }
+        }
+    }
+    if(f==0 && c<=m)
+        return 1;
+    return 0;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    freopen("in.txt","r",stdin);
+//    freopen("out.txt","w",stdout);
+
+    int tks,ks=1;
+    cin>>tks;
+//    scanf("%d",&tks);
+    while(tks--)
+    {
+        cin>>n>>m;
+        for(int i=0;i<n;i++)
+            cin>>ar[i];
+        LL l=0,r=1e9 ;
+        while(r>l)
+        {
+            LL mid=l+(r-l)/2;
+            if(fun(mid))
+            {
+                r=mid;
+            }
+            else
+                l=mid+1;
+        }
+        cout<<"Case "<<ks++<<": "<<r<<en;
+//        printf("Case %d: ",ks++);
+    }
+//
+//    while(1 == scanf("%d",&n) && n)
+//    {
+////        printf("\n");
+//    }
+    return 0;
+}
